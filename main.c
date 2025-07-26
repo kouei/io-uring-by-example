593d54cb31c188e8f6d7d0026e12fcb837458c73
kouei <kagigokoro@gmail.com>
2025-07-26 11:23:09 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,259 +1,451 @@
-#include <assert.h>
-#include <errno.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <liburing.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define QUEUE_DEPTH 32
-#define BLOCK_SZ (16 * 1024)
-#define min(x, y) ((x) < (y) ? (x) : (y))
+#define SERVER_STRING "Server: zerohttpd/0.1\r\n"
+#define DEFAULT_SERVER_PORT 8000
+#define QUEUE_DEPTH 256
+#define READ_SZ 8192
 
-static int infd;
-static int outfd;
-static struct io_uring ring;
-
-struct io_task {
-  bool is_read;
-  off_t initial_offset;
-  off_t offset;
-  size_t initial_len;
-  struct iovec iov;
-  char bytes[0]; /* Flexible Array Member. Real Data Payload. */
+enum event_type_t {
+  EVENT_TYPE_ACCEPT,
+  EVENT_TYPE_READ,
+  EVENT_TYPE_WRITE,
 };
 
-static off_t get_file_size(int fd) {
-  struct stat st;
+struct request {
+  enum event_type_t event_type;
+  int iovec_count;
+  int client_socket;
+  struct iovec iov[0];
+};
 
-  if (fstat(fd, &st) < 0) {
-    fprintf(stderr, "fstat() failed.");
-    exit(-1);
-  }
+struct io_uring ring;
 
-  if (S_ISREG(st.st_mode)) {
-    return st.st_size;
-  }
+const char *unimplemented_content =
+    "HTTP/1.0 400 Bad Request\r\n"
+    "Content-type: text/html\r\n"
+    "\r\n"
+    "<html>"
+    "<head>"
+    "<title>ZeroHTTPd: Unimplemented</title>"
+    "</head>"
+    "<body>"
+    "<h1>Bad Request (Unimplemented)</h1>"
+    "<p>Your client sent a request ZeroHTTPd did not understand and it is "
+    "probably not your fault.</p>"
+    "</body>"
+    "</html>";
 
-  if (S_ISBLK(st.st_mode)) {
-    off_t bytes;
-    if (ioctl(fd, BLKGETSIZE64, &bytes) != 0) {
-      fprintf(stderr, "ioctl() failed.");
-      exit(-1);
-    }
+const char *http_404_content = "HTTP/1.0 404 Not Found\r\n"
+                               "Content-type: text/html\r\n"
+                               "\r\n"
+                               "<html>"
+                               "<head>"
+                               "<title>ZeroHTTPd: Not Found</title>"
+                               "</head>"
+                               "<body>"
+                               "<h1>Not Found (404)</h1>"
+                               "<p>Your client is asking for an object that "
+                               "was not found on this server.</p>"
+                               "</body>"
+                               "</html>";
 
-    return bytes;
-  }
+/*
+ * Utility function to convert a string to lower case.
+ * */
 
-  fprintf(stderr, "Unsupported st_mode = %u", st.st_mode);
-  exit(-1);
+void strtolower(char *str) {
+  for (; *str; ++str)
+    *str = (char)tolower(*str);
+}
+/*
+ One function that prints the system call and the error details
+ and then exits with error code 1. Non-zero meaning things didn't go well.
+ */
+void fatal_error(const char *syscall) {
+  perror(syscall);
+  exit(1);
 }
 
-static void requeue_task(struct io_task *task) {
-  struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
-  if (sqe == NULL) {
-    fprintf(stderr, "io_uring_get_sqe() failed.");
-    exit(-1);
-  }
+/*
+ * Helper function for cleaner looking code.
+ * */
 
-  if (task->is_read) {
-    io_uring_prep_readv(sqe, infd, &task->iov, 1, task->offset);
-  } else {
-    io_uring_prep_writev(sqe, outfd, &task->iov, 1, task->offset);
+void *zh_malloc(size_t size) {
+  void *buf = malloc(size);
+  if (!buf) {
+    fprintf(stderr, "Fatal error: unable to allocate memory.\n");
+    exit(1);
   }
-
-  io_uring_sqe_set_data(sqe, task);
+  return buf;
 }
 
-static int queue_read(off_t size, off_t offset) {
+/*
+ * This function is responsible for setting up the main listening socket used by
+ * the web server.
+ * */
+
+int setup_listening_socket(int port) {
+  int sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (sock == -1) {
+    fatal_error("socket()");
+  }
+
+  int enable = 1;
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
+    fatal_error("setsockopt(SO_REUSEADDR)");
+  }
+
+  struct sockaddr_in srv_addr = {};
+  srv_addr.sin_family = AF_INET;
+  srv_addr.sin_port = htons(port);
+  srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  /* We bind to a port and turn this socket into a listening
+   * socket.
+   * */
+  if (bind(sock, (const struct sockaddr *)&srv_addr, sizeof(srv_addr)) < 0) {
+    fatal_error("bind()");
+  }
+
+  if (listen(sock, 10) < 0) {
+    fatal_error("listen()");
+  }
+
+  return sock;
+}
+
+int add_accept_request(int server_socket, struct sockaddr_in *client_addr,
+                       socklen_t *client_addr_len) {
   struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
-  if (sqe == NULL) {
-    return -1;
-  }
+  io_uring_prep_accept(sqe, server_socket, (struct sockaddr *)client_addr,
+                       client_addr_len, 0);
+  struct request *req = malloc(sizeof(*req));
+  req->event_type = EVENT_TYPE_ACCEPT;
+  io_uring_sqe_set_data(sqe, req);
+  io_uring_submit(&ring);
 
-  struct io_task *task = malloc(sizeof(*task) + size);
-  if (!task) {
-    return -1;
-  }
-
-  task->is_read = true;
-  task->initial_offset = offset;
-  task->offset = offset;
-  task->initial_len = size;
-
-  task->iov.iov_base = task->bytes;
-  task->iov.iov_len = task->initial_len;
-
-  io_uring_prep_readv(sqe, infd, &task->iov, 1, offset);
-  io_uring_sqe_set_data(sqe, task);
   return 0;
 }
 
-static void queue_write(struct io_task *task) {
+int add_read_request(int client_socket) {
   struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
-  if (sqe == NULL) {
-    fprintf(stderr, "io_uring_get_sqe() failed.");
-    exit(-1);
-  }
-
-  task->is_read = false;
-  task->offset = task->initial_offset;
-
-  task->iov.iov_base = task->bytes;
-  task->iov.iov_len = task->initial_len;
-
-  io_uring_prep_writev(sqe, outfd, &task->iov, 1, task->offset);
-  io_uring_sqe_set_data(sqe, task);
+  struct request *req = malloc(sizeof(*req) + sizeof(struct iovec));
+  req->iov[0].iov_base = malloc(READ_SZ);
+  req->iov[0].iov_len = READ_SZ;
+  req->event_type = EVENT_TYPE_READ;
+  req->client_socket = client_socket;
+  memset(req->iov[0].iov_base, 0, READ_SZ);
+  /* Linux kernel 5.5 has support for readv, but not for recv() or read() */
+  io_uring_prep_readv(sqe, client_socket, &req->iov[0], 1, 0);
+  io_uring_sqe_set_data(sqe, req);
+  io_uring_submit(&ring);
+  return 0;
 }
 
-void spawn_read_tasks(unsigned long *read_tasks, unsigned long *write_tasks,
-                      off_t *bytes_to_read, off_t *read_offset) {
-  /* Queue up as many reads as we can */
-  unsigned long previous_read_tasks = *read_tasks;
-  while (*bytes_to_read > 0) {
-    if (*read_tasks + *write_tasks >= QUEUE_DEPTH) {
-      break;
-    }
-
-    off_t read_size = min(*bytes_to_read, BLOCK_SZ);
-
-    int ret = queue_read(read_size, *read_offset);
-    if (ret < 0) {
-      break;
-    }
-
-    *bytes_to_read -= read_size;
-    *read_offset += read_size;
-    *read_tasks += 1;
-  }
-
-  if (previous_read_tasks < *read_tasks) {
-    int ret = io_uring_submit(&ring);
-    if (ret < 0) {
-      fprintf(stderr, "io_uring_submit failed: %s\n", strerror(-ret));
-      exit(-1);
-    }
-  }
+int add_write_request(struct request *req) {
+  struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+  req->event_type = EVENT_TYPE_WRITE;
+  io_uring_prep_writev(sqe, req->client_socket, req->iov, req->iovec_count, 0);
+  io_uring_sqe_set_data(sqe, req);
+  io_uring_submit(&ring);
+  return 0;
 }
 
-void spawn_write_tasks(unsigned long *read_tasks, unsigned long *write_tasks,
-                       off_t *bytes_to_write) {
-  /* Queue is full at this point. Let's find at least one completion */
-  bool already_found_completed_task = false;
-  while (*bytes_to_write > 0) {
-    struct io_uring_cqe *cqe;
-    if (!already_found_completed_task) {
-      int ret = io_uring_wait_cqe(&ring, &cqe);
-      if (ret < 0) {
-        fprintf(stderr, "io_uring_wait_cqe failed: %s\n", strerror(-ret));
-        exit(-1);
-      }
+void _send_static_string_content(const char *str, int client_socket) {
+  struct request *req = zh_malloc(sizeof(*req) + sizeof(struct iovec));
+  unsigned long slen = strlen(str);
+  req->iovec_count = 1;
+  req->client_socket = client_socket;
+  req->iov[0].iov_base = zh_malloc(slen);
+  req->iov[0].iov_len = slen;
+  memcpy(req->iov[0].iov_base, str, slen);
+  add_write_request(req);
+}
 
-      already_found_completed_task = true;
+/*
+ * When ZeroHTTPd encounters any other HTTP method other than GET or POST, this
+ * function is used to inform the client.
+ * */
+
+void handle_unimplemented_method(int client_socket) {
+  _send_static_string_content(unimplemented_content, client_socket);
+}
+
+/*
+ * This function is used to send a "HTTP Not Found" code and message to the
+ * client in case the file requested is not found.
+ * */
+
+void handle_http_404(int client_socket) {
+  _send_static_string_content(http_404_content, client_socket);
+}
+
+/*
+ * Once a static file is identified to be served, this function is used to read
+ * the file and write it over the client socket using Linux's sendfile() system
+ * call. This saves us the hassle of transferring file buffers from kernel to
+ * user space and back.
+ * */
+
+void copy_file_contents(char *file_path, off_t file_size, struct iovec *iov) {
+  int fd;
+
+  char *buf = zh_malloc(file_size);
+  fd = open(file_path, O_RDONLY);
+  if (fd < 0)
+    fatal_error("read");
+
+  /* We should really check for short reads here */
+  int ret = read(fd, buf, file_size);
+  if (ret < file_size) {
+    fprintf(stderr, "Encountered a short read.\n");
+  }
+  close(fd);
+
+  iov->iov_base = buf;
+  iov->iov_len = file_size;
+}
+
+/*
+ * Simple function to get the file extension of the file that we are about to
+ * serve.
+ * */
+
+const char *get_filename_ext(const char *filename) {
+  const char *dot = strrchr(filename, '.');
+  if (!dot || dot == filename)
+    return "";
+  return dot + 1;
+}
+
+/*
+ * Sends the HTTP 200 OK header, the server string, for a few types of files, it
+ * can also send the content type based on the file extension. It also sends the
+ * content length header. Finally it send a '\r\n' in a line by itself
+ * signalling the end of headers and the beginning of any content.
+ * */
+
+void send_headers(const char *path, off_t len, struct iovec *iov) {
+  char small_case_path[1024];
+  char send_buffer[1024];
+  strcpy(small_case_path, path);
+  strtolower(small_case_path);
+
+  char *str = "HTTP/1.0 200 OK\r\n";
+  unsigned long slen = strlen(str);
+  iov[0].iov_base = zh_malloc(slen);
+  iov[0].iov_len = slen;
+  memcpy(iov[0].iov_base, str, slen);
+
+  slen = strlen(SERVER_STRING);
+  iov[1].iov_base = zh_malloc(slen);
+  iov[1].iov_len = slen;
+  memcpy(iov[1].iov_base, SERVER_STRING, slen);
+
+  /*
+   * Check the file extension for certain common types of files
+   * on web pages and send the appropriate content-type header.
+   * Since extensions can be mixed case like JPG, jpg or Jpg,
+   * we turn the extension into lower case before checking.
+   * */
+  const char *file_ext = get_filename_ext(small_case_path);
+  if (strcmp("jpg", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: image/jpeg\r\n");
+  if (strcmp("jpeg", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: image/jpeg\r\n");
+  if (strcmp("png", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: image/png\r\n");
+  if (strcmp("gif", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: image/gif\r\n");
+  if (strcmp("htm", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: text/html\r\n");
+  if (strcmp("html", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: text/html\r\n");
+  if (strcmp("js", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: application/javascript\r\n");
+  if (strcmp("css", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: text/css\r\n");
+  if (strcmp("txt", file_ext) == 0)
+    strcpy(send_buffer, "Content-Type: text/plain\r\n");
+  slen = strlen(send_buffer);
+  iov[2].iov_base = zh_malloc(slen);
+  iov[2].iov_len = slen;
+  memcpy(iov[2].iov_base, send_buffer, slen);
+
+  /* Send the content-length header, which is the file size in this case. */
+  sprintf(send_buffer, "content-length: %ld\r\n", len);
+  slen = strlen(send_buffer);
+  iov[3].iov_base = zh_malloc(slen);
+  iov[3].iov_len = slen;
+  memcpy(iov[3].iov_base, send_buffer, slen);
+
+  /*
+   * When the browser sees a '\r\n' sequence in a line on its own,
+   * it understands there are no more headers. Content may follow.
+   * */
+  strcpy(send_buffer, "\r\n");
+  slen = strlen(send_buffer);
+  iov[4].iov_base = zh_malloc(slen);
+  iov[4].iov_len = slen;
+  memcpy(iov[4].iov_base, send_buffer, slen);
+}
+
+void handle_get_method(char *path, int client_socket) {
+  char final_path[1024];
+
+  /*
+   If a path ends in a trailing slash, the client probably wants the index
+   file inside of that directory.
+   */
+  if (path[strlen(path) - 1] == '/') {
+    strcpy(final_path, "public");
+    strcat(final_path, path);
+    strcat(final_path, "index.html");
+  } else {
+    strcpy(final_path, "public");
+    strcat(final_path, path);
+  }
+
+  /* The stat() system call will give you information about the file
+   * like type (regular file, directory, etc), size, etc. */
+  struct stat path_stat;
+  if (stat(final_path, &path_stat) == -1) {
+    printf("404 Not Found: %s (%s)\n", final_path, path);
+    handle_http_404(client_socket);
+  } else {
+    /* Check if this is a normal/regular file and not a directory or something
+     * else */
+    if (S_ISREG(path_stat.st_mode)) {
+      struct request *req =
+          zh_malloc(sizeof(*req) + (sizeof(struct iovec) * 6));
+      req->iovec_count = 6;
+      req->client_socket = client_socket;
+      send_headers(final_path, path_stat.st_size, req->iov);
+      copy_file_contents(final_path, path_stat.st_size, &req->iov[5]);
+      printf("200 %s %ld bytes\n", final_path, path_stat.st_size);
+      add_write_request(req);
     } else {
-      int ret = io_uring_peek_cqe(&ring, &cqe);
-      if (ret == -EAGAIN) { // EAGAIN means retry. It also means currently CQ
-                            // is empty.
+      handle_http_404(client_socket);
+      printf("404 Not Found: %s\n", final_path);
+    }
+  }
+}
+
+/*
+ * This function looks at method used and calls the appropriate handler
+ * function. Since we only implement GET and POST methods, it calls
+ * handle_unimplemented_method() in case both these don't match. This sends an
+ * error to the client.
+ * */
+
+void handle_http_method(char *method_buffer, int client_socket) {
+  char *method, *path, *saveptr;
+
+  method = strtok_r(method_buffer, " ", &saveptr);
+  strtolower(method);
+  path = strtok_r(NULL, " ", &saveptr);
+
+  if (strcmp(method, "get") == 0) {
+    handle_get_method(path, client_socket);
+  } else {
+    handle_unimplemented_method(client_socket);
+  }
+}
+
+int get_line(const char *src, char *dest, int dest_sz) {
+  for (int i = 0; i < dest_sz; i++) {
+    dest[i] = src[i];
+    if (src[i] == '\r' && src[i + 1] == '\n') {
+      dest[i] = '\0';
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int handle_client_request(struct request *req) {
+  char http_request[1024];
+  /* Get the first line, which will be the request */
+  if (get_line(req->iov[0].iov_base, http_request, sizeof(http_request))) {
+    fprintf(stderr, "Malformed request\n");
+    exit(1);
+  }
+  handle_http_method(http_request, req->client_socket);
+  return 0;
+}
+
+void server_loop(int server_socket) {
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+
+  add_accept_request(server_socket, &client_addr, &client_addr_len);
+
+  while (true) {
+    struct io_uring_cqe *cqe;
+    int ret = io_uring_wait_cqe(&ring, &cqe);
+    if (ret < 0) {
+      fatal_error("io_uring_wait_cqe");
+    }
+
+    struct request *req = (struct request *)cqe->user_data;
+    if (cqe->res < 0) {
+      fprintf(stderr, "Async request failed: %s for event: %d\n",
+              strerror(-cqe->res), req->event_type);
+      exit(1);
+    }
+
+    switch (req->event_type) {
+    case EVENT_TYPE_ACCEPT:
+      add_accept_request(server_socket, &client_addr, &client_addr_len);
+      add_read_request(cqe->res);
+      break;
+    case EVENT_TYPE_READ:
+      if (!cqe->res) {
+        fprintf(stderr, "Empty request!\n");
         break;
       }
-
-      if (ret < 0) {
-        fprintf(stderr, "io_uring_peek_cqe failed: %s\n", strerror(-ret));
-        exit(-1);
+      handle_client_request(req);
+      free(req->iov[0].iov_base);
+      break;
+    case EVENT_TYPE_WRITE:
+      for (int i = 0; i < req->iovec_count; i++) {
+        free(req->iov[i].iov_base);
       }
+      close(req->client_socket);
+      break;
+    default:
+      fprintf(stderr, "Unexpected event type = %d\n", req->event_type);
+      break;
     }
 
-    struct io_task *task = io_uring_cqe_get_data(cqe);
-    if (cqe->res == -EAGAIN) { // EAGAIN means retry.
-      requeue_task(task);
-      /* Notify kernel that a CQE has been consumed successfully. */
-      io_uring_cqe_seen(&ring, cqe);
-      continue;
-    }
-
-    if (cqe->res < 0) {
-      fprintf(stderr, "cqe failed: %s\n", strerror(-cqe->res));
-      exit(-1);
-    }
-
-    if (cqe->res != task->iov.iov_len) {
-      /* short read/write; adjust and requeue */
-      task->iov.iov_base += cqe->res;
-      task->iov.iov_len -= cqe->res;
-      requeue_task(task);
-      /* Notify kernel that a CQE has been consumed successfully. */
-      io_uring_cqe_seen(&ring, cqe);
-      continue;
-    }
-
-    /*
-     * All done. If write, nothing else to do. If read,
-     * queue up corresponding write.
-     * */
-    if (task->is_read) {
-      queue_write(task);
-      io_uring_submit(&ring);
-      *read_tasks -= 1;
-      *write_tasks += 1;
-    } else {
-      *bytes_to_write -= task->initial_len;
-      free(task);
-      *write_tasks -= 1;
-    }
-
+    free(req);
+    /* Mark this request as processed */
     io_uring_cqe_seen(&ring, cqe);
   }
 }
 
-void copy_file(off_t file_size) {
-  off_t bytes_to_read = file_size;
-  off_t read_offset = 0;
-
-  off_t bytes_to_write = file_size;
-
-  unsigned long read_tasks = 0;
-  unsigned long write_tasks = 0;
-
-  while (bytes_to_read > 0 || bytes_to_write > 0) {
-    spawn_read_tasks(&read_tasks, &write_tasks, &bytes_to_read, &read_offset);
-    spawn_write_tasks(&read_tasks, &write_tasks, &bytes_to_write);
-  }
+void sigint_handler(int signo) {
+  printf("^C pressed. Shutting down.\n");
+  io_uring_queue_exit(&ring);
+  exit(0);
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    printf("Usage: %s <infile> <outfile>\n", argv[0]);
-    exit(-1);
-  }
+int main() {
+  signal(SIGINT, sigint_handler);
 
-  infd = open(argv[1], O_RDONLY);
-  if (infd < 0) {
-    fprintf(stderr, "open infile failed");
-    exit(-1);
-  }
+  io_uring_queue_init(QUEUE_DEPTH, &ring, 0);
 
-  outfd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-  if (outfd < 0) {
-    fprintf(stderr, "open outfile failed");
-    exit(-1);
-  }
-
-  int ret = io_uring_queue_init(QUEUE_DEPTH, &ring, 0);
-  if (ret < 0) {
-    fprintf(stderr, "io_uring_queue_init failed: %s\n", strerror(-ret));
-    exit(-1);
-  }
-
-  off_t insize = get_file_size(infd);
-
-  copy_file(insize);
-
-  io_uring_queue_exit(&ring);
-  close(outfd);
-  close(infd);
+  int server_socket = setup_listening_socket(DEFAULT_SERVER_PORT);
+  server_loop(server_socket);
 
   return 0;
 }
